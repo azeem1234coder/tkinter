c1104b12c82fdf4803341db5f4a1496257abb12d
azeem1234coder <odusanyaabdulazeem25@gmail.com>
2025-04-06 12:23:47 -0700

    33

---
+++ dd.py
@@ -0,0 +1,13 @@
+# Import necessary libraries
+from tkinter import *
+
+#Create Window 
+window = Tk()
+
+#Set the window Title and Geometry 
+
+window.title('Demo Window')
+
+
+window.geometry('400x300')
+window.mainloop()
